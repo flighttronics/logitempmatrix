91f0f085e2fa49fa55d37fde7d24c7909bb66de8
flighttronics <andreas@flighttronics.se>
2015-05-01 23:00:22 +0200

    First test run with curlcpp

--- src/logitempmatrix.cc
+++ src/logitempmatrix.cc
@@ -5,19 +5,30 @@
 // This code is public domain
 // (but note, that the led-matrix library this depends on is GPL v2)
 
-#include "led-matrix.h"
-
 #include <unistd.h>
 #include <math.h>
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <string.h>
 #include <stdlib.h>
+#include "led-matrix.h"
+#include "curl_easy.h"
+#include "curl_option.h"
 
 using namespace std;
 using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
+using curl::curl_easy;
+using curl::make_option;
+
+struct MemoryStruct {
+	char *memory;
+	size_t size;
+};
+
+static float temps[9];
 
 static void _DrawTankTempGradient(Canvas *canvas, unsigned int tank,
 		float temps[]) {
@@ -51,9 +62,34 @@ static void _DrawTankTempGradient(Canvas *canvas, unsigned int tank,
 		b = 0;
 
 		for (i = 0; i < 7; i++) {
-			canvas->SetPixel(3 + (tank * 10)+ i, j, r, g, b);
+			canvas->SetPixel(3 + (tank * 10) + i, j, r, g, b);
 		}
 	}
+}
+
+static size_t WriteMemoryCallback(char *contents, size_t size, size_t nmemb,
+		void *userp) {
+
+	size_t i, j;
+	string str = "";
+
+	j = 0;
+	for (i = 0; i < nmemb; i++) {
+		if (contents[i] == '\n') {
+			try {
+				temps[j] = stof(str);
+				cout << "f=" << temps[j] << endl;
+				j++;
+				str = "";
+			} catch (...) {
+				str = "";
+			}
+		} else {
+			str = str + contents[i];
+		}
+	}
+
+	return nmemb;
 }
 
 static void _DrawTankGradients(Canvas *canvas) {
@@ -63,24 +99,26 @@ static void _DrawTankGradients(Canvas *canvas) {
 	 */
 
 	int i;
-	float temps[3 * 3];
-	string str;
+	curl_easy easy;
+	char data[1024];
+
+	easy.add<CURLOPT_URL>("http://192.168.0.104:14007/temps.txt");
+	easy.add<CURLOPT_WRITEFUNCTION>(WriteMemoryCallback);
+	easy.add<CURLOPT_WRITEDATA>(data);
+	easy.add<CURLOPT_USERAGENT>("libcurl-agent/1.0");
+	easy.add<CURLOPT_FOLLOWLOCATION>(1L);
 
 	while (1) {
 
-		remove("temps.txt");
-		system("wget http://192.168.0.104:14007/temps.txt");
-
-		fstream myFile("temps.txt", std::ios_base::in);
-
-		getline(myFile, str);
-		cout << str << endl;
-
-		for (i = 0; i < 9; i++) {
-			myFile >> temps[i];
+		try {
+			easy.perform();
+		} catch (curl_easy_exception error) {
+			// If you want to get the entire error stack we can do:
+			//auto errors = error.what();
+			// Otherwise we could print the stack like this:
+			error.print_traceback();
+			// Note that the printing the stack will erase it
 		}
-
-		myFile.close();
 
 		_DrawTankTempGradient(canvas, 0, &temps[6]);
 		_DrawTankTempGradient(canvas, 1, &temps[0]);
@@ -92,29 +130,27 @@ static void _DrawTankGradients(Canvas *canvas) {
 	}
 }
 
-int main(int argc, char *argv[]) {
-	/*
-	 * Set up GPIO pins. This fails when not running as root.
-	 */
-	GPIO io;
-	if (!io.Init())
-		return 1;
+int main(void) {
+/*
+ * Set up GPIO pins. This fails when not running as root.
+ */
+GPIO io;
+if (!io.Init())
+	return 1;
 
-	cout << "Hello world" << endl;
+/*
+ * Set up the RGBMatrix. It implements a 'Canvas' interface.
+ */
+int rows = 32;    // A 32x32 display. Use 16 when this is a 16x32 display.
+int chain = 2;    // Number of boards chained together.
+int parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
+Canvas *canvas = new RGBMatrix(&io, rows, chain, parallel);
 
-	/*
-	 * Set up the RGBMatrix. It implements a 'Canvas' interface.
-	 */
-	int rows = 32;    // A 32x32 display. Use 16 when this is a 16x32 display.
-	int chain = 2;    // Number of boards chained together.
-	int parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
-	Canvas *canvas = new RGBMatrix(&io, rows, chain, parallel);
+_DrawTankGradients(canvas);    // Using the canvas.
 
-	_DrawTankGradients(canvas);    // Using the canvas.
+// Animation finished. Shut down the RGB matrix.
+//canvas->Clear();
+delete canvas;
 
-	// Animation finished. Shut down the RGB matrix.
-	//canvas->Clear();
-	delete canvas;
-
-	return 0;
+return 0;
 }
